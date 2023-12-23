2af1109bb41dbfd2bcf00a9a27d8b55837ed33c6
sutigit <panyanilben@gmail.com>
2023-12-23 15:22:27 +0200

    add more routes

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
